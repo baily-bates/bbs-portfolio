d7689ab331353b4f56b119e00c3b26a14eb69bdd
Baily Bates <BnBates1@student.fullsail.edu>
2025-11-01 15:07:14 -0500

    made slight adjustment in NavBar

--- Desktop/bbs-portfolio/src/components/NavBar.jsx
+++ Desktop/bbs-portfolio/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/logo.png";
-import styles from "./Navbar.module.css";
+import styles from "./NavBar.module.css";
 
 const NavBar = () => {
   return (
